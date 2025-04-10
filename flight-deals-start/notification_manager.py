3070dac7e3bd065fff8e6ac714cbd902b949104f
Oluwafemi0908 <oluwafemiakinode09@gmail.com>
2025-04-10 22:54:07 +0100

    Update notification_manager.py

--- flight-deals-start/notification_manager.py
+++ flight-deals-start/notification_manager.py
@@ -4,13 +4,13 @@ from twilio.rest import Client
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     def __init__(self, message_body):
-        account_sid = 'AC1498b39891ab497125a713db481d9e72'
-        auth_token = '61592578095c2a266e5873de4608a1eb'
+        account_sid = 'TWILLIO SID'
+        auth_token = 'TWILLIO TOKEN'
         client = Client(account_sid, auth_token)
         message = client.messages.create(
-          from_='+12513354300',
+          from_='TWILLIO NUMBER',
           body=message_body,
-          to='+2349055789508'
+          to='RECEIVER's NUMBER'
         )
 
         print(message.sid)
